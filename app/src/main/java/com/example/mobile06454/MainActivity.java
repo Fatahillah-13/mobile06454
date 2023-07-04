46f8f20bdb1e35632ea4b70696ac96e9381f4534
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2023-07-04 11:04:44 +0700

    Input Data

--- app/src/main/java/com/example/mobile06454/MainActivity.java
+++ app/src/main/java/com/example/mobile06454/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.mobile06454;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edkode, ednama, edsatuan, edharga;
+    DatabaseReference dbr;
+    Button tombolsave;
+    ModelBarang modelBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edkode=findViewById(R.id.kodebrg);
+        ednama=findViewById(R.id.namabrg);
+        edsatuan=findViewById(R.id.satuanbrg);
+        edharga=findViewById(R.id.hargabrg);
+        modelBarang=new ModelBarang();
+        dbr= FirebaseDatabase.getInstance().getReference().child("StokBarang");
+
+        tombolsave=findViewById(R.id.tombolsimpan);
+        tombolsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modelBarang.setKode(edkode.getText().toString());
+                modelBarang.setNama_brg(ednama.getText().toString());
+                modelBarang.setSatuan(edsatuan.getText().toString());
+                modelBarang.setHarga(Integer.parseInt(edharga.getText().toString()));
+
+                dbr.push().setValue(modelBarang);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mobile06454/ModelBarang.java
@@ -0,0 +1,40 @@
+package com.example.mobile06454;
+
+public class ModelBarang {
+    String kode;
+    String nama_brg;
+    String satuan;
+    int harga;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama_brg() {
+        return nama_brg;
+    }
+
+    public void setNama_brg(String nama_brg) {
+        this.nama_brg = nama_brg;
+    }
+
+    public String getSatuan() {
+        return satuan;
+    }
+
+    public void setSatuan(String satuan) {
+        this.satuan = satuan;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
